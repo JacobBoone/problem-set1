9ecf184f04875efdb33ee5cc08c2a28cfd3aa2e0
Jacob <jakeboone@aol.com>
2014-07-02 16:19:02 -0600

    progress on part 1

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 $(document).on('ready', function() {
   
+var letterCapitalize = function(sentance){
+
+	sentance = sentance.split(" ");
+
+	for (var i = 0; i <sentance.length; i++) {
+		for (var j = 0; j <sentance[i].length; j++) {
+			var caps
+
+			if (j===0){
+				 caps = sentance[i][j].toUpperCase();
+			}
+			else {
+				caps += sentance[i][j];
+			}
+		}
+
+	};
+	return caps
+	// var caps = sentance[0].toUpperCase();
+
+	// for (var i = 1; i<sentance.length; i++) {
+	// 	caps += sentance[i]
+	// 	};
+	
+	// console.log(caps);
+
+}
+letterCapitalize('jon like pizza')
+
+
+
+
+
+
+
+
 });
+
+
+var wordCount = function(words){
+
+
+}
